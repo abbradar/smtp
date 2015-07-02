d42b95988a8c09a556544bab77f4ffb0ff9d046b
Nikolay Amiantov <ab@fmap.me>
2015-07-03 01:51:44 +0300

    added nix files

---
+++ default.nix
@@ -0,0 +1,14 @@
+{ mkDerivation, attoparsec, base, bytestring, conduit
+, conduit-extra, netwire, network, stdenv, text, transformers
+, vector
+}:
+mkDerivation {
+  pname = "smtp";
+  version = "0.1.0.0";
+  src = ./.;
+  buildDepends = [
+    attoparsec base bytestring conduit conduit-extra netwire network
+    text transformers vector
+  ];
+  license = stdenv.lib.licenses.bsd3;
+}
